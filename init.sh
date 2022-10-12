09e7b2c646a5800491c850879d3d3b94ffb807fd
xxc <714852076@qq.com>
2022-10-12 15:19:01 +0800

    日常修改

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#mv $HOME/.config/i3 $HOME/.config/i3.back
+#ln -s $HOME/桌面/dotfiles/i3 $HOME/.config/i3
+
+mv $HOME/.config/polybar $HOME/.config/polybar.back
+
+ln -s $HOME/桌面/dotfiles/polybar $HOME/.config/polybar
+
+
